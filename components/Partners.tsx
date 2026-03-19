57f48d897a08d9a824761222f181cca0426ea9b3
annandiaye11 <anna.ndiaye1@ism.edu.sn>
2026-03-19 15:43:02 +0000

    fix: update logo URLs for Wave, Mansa Bank, BGFI Bank, Crédit du Sénégal, Teyliom Group

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 const partners = [
-  { name: "BGFI Bank",         logo: "https://logo.clearbit.com/bgfi-bank.com" },
-  { name: "Teyliom Group",     logo: "https://logo.clearbit.com/teyliom.com" },
+  { name: "BGFI Bank",         logo: "https://play-lh.googleusercontent.com/bGTBVAZjP_HcZtiDVRylY7hGPp5l9wpLt2GZydVSzOIh4ZVdUtNpxRsZdvnSdn6VqJGF=w3840-h2160-rw" },
+  { name: "Teyliom Group",     logo: "https://upload.wikimedia.org/wikipedia/commons/9/96/Logo_Teyliom-Group.png" },
   { name: "Ecobank",           logo: "https://upload.wikimedia.org/wikipedia/commons/9/98/Ecobank_Logo.svg" },
   { name: "NSIA Banque",       logo: "https://logo.clearbit.com/nsia-ci.com" },
   { name: "MTN",               logo: "https://upload.wikimedia.org/wikipedia/commons/a/af/MTN_Logo.svg" },
-  { name: "Crédit du Sénégal", logo: "https://logo.clearbit.com/creditdusenegal.sn" },
+  { name: "Crédit du Sénégal", logo: "https://apitasolution.com/wp-content/uploads/2025/09/logo.png" },
   { name: "Moov Africa",       logo: "https://logo.clearbit.com/moov.africa" },
   { name: "Orange",            logo: "https://upload.wikimedia.org/wikipedia/commons/c/c8/Orange_logo.svg" },
-  { name: "Wave",              logo: "https://logo.clearbit.com/wave.com" },
+  { name: "Wave",              logo: "https://www.financialafrik.com/wp-content/uploads/2022/07/63915358-45975666.jpg" },
   { name: "UBA",               logo: "https://upload.wikimedia.org/wikipedia/en/thumb/2/2c/United_Bank_for_Africa_logo.svg/250px-United_Bank_for_Africa_logo.svg.png" },
   { name: "Société Générale",  logo: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Soci%C3%A9t%C3%A9_G%C3%A9n%C3%A9rale.svg" },
   { name: "Attijariwafa Bank", logo: "https://upload.wikimedia.org/wikipedia/commons/1/1c/LogoNew.png" },
   { name: "Coris Bank",        logo: "https://upload.wikimedia.org/wikipedia/commons/2/23/Logo_Coris_Bank.svg" },
   { name: "BNP Paribas",       logo: "https://upload.wikimedia.org/wikipedia/commons/6/6a/BNP_Paribas.svg" },
-  { name: "Mansa Bank",        logo: "https://logo.clearbit.com/mansa.bank" },
+  { name: "Mansa Bank",        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxV7FW-rSkCrNLObKghC_iaSDn3oL1UcYvPA&s" },
   { name: "Access Bank",       logo: "https://upload.wikimedia.org/wikipedia/commons/1/14/Access_Bank_Logo.png" },
 ];
 
